5abc0ecf5720fcdd94b17e5230254c5a758e61de
ashim.kc <ashim.kc@f1soft.com>
2025-06-03 18:15:32 +0545

    Added transaction summary, delete api

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const PORT = process.env.PORT || 3000;
 const app =  express();
 
 app.use(express.json());
+app.get('/healthcheck', (req, res) => {
+    return res.status(200).json({ status: 'ok' });
+  });
 
 app.use('/api', routes);
 
